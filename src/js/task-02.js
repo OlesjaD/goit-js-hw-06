9223a3682f93e16c4d399d0f0665fe6d2f957d4a
OlesjaD <olesja.d.c@gmail.com>
2023-02-22 17:12:55 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredient = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const element = ingredients[i];
+
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = ingredients[i];
+  ingredientsEl.classList.add('item');
+
+  ingredient.push(ingredientsEl);
+}
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...ingredient);
+console.log(listEl);
