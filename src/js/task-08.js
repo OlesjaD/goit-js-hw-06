d333f4f65f664fe595be28f426c5ab8befdd98db
OlesjaD <olesja.d.c@gmail.com>
2023-02-25 22:40:06 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,34 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    
+    if (email === "" || password === "") {
+        form.textContent = "Будь ласка, заповніть всі поля форми!";
+    }  else { const formData = {email, password};
+        console.log(formData);
+    };
+
+      event.currentTarget.reset();
+};
+
+// form[0].setAttribute("required", "");
+// form[1].setAttribute("required", "");
+
+    // const formData = new FormData(event.currentTarget);
+
+    // formData.forEach((value, name) => {
+    //     const formValue = {
+    //         name,
+    //         value,
+    //     };
+    //     console.log(formValue);
+    // }) 
