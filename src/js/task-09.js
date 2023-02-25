756e53eadddc310c5ae85840016f8d0fbc200a34
OlesjaD <olesja.d.c@gmail.com>
2023-02-25 23:09:39 +0200

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector('body');
+
+const textRef = document.querySelector('.color');
+
+const btnRef = document.querySelector('.change-color');
+
+btnRef.addEventListener('click', onClickButton);
+
+function onClickButton () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  textRef.textContent = getRandomHexColor("value");
+};
