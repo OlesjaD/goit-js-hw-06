1367ab779ce9ded3393b57e43d003a723ee90177
OlesjaD <olesja.d.c@gmail.com>
2023-02-27 00:41:30 +0200

    task01

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,11 @@
 
-const ulElem = document.querySelectorAll('.item');
-console.log('Number of categories:', ulElem.length);
-
-
 const itemEl = document.querySelector('#categories');
-const firstEl = itemEl.firstElementChild;
-const titlefirstEl = firstEl.firstElementChild;
-console.log('Category:', titlefirstEl.textContent);
-const sibFirstEl = titlefirstEl.nextElementSibling;
-console.log('Elements:', sibFirstEl.children.length);
+const ulElem = document.querySelectorAll('.item');
 
+console.log('Number of categories:', itemEl.children.length);
 
-const nextEl = firstEl.nextElementSibling;
-const titlenextEl = nextEl.firstElementChild;
-console.log('Category:', titlenextEl.textContent);
-const sibNextEl = titlenextEl.nextElementSibling;
-console.log('Elements:', sibNextEl.children.length);
-
-
-const lastEl = itemEl.lastElementChild;
-const titlelastEl = lastEl.firstElementChild;
-console.log('Category:', titlelastEl.textContent);
-const sibLastEl = titlelastEl.nextElementSibling;
-console.log('Elements:', sibLastEl.children.length);
+for (let i = 0; i < ulElem.length; i++) {
+    const element = ulElem[i];
+console.log('Category:', element.firstElementChild.textContent);
+console.log('Elements:', element.lastElementChild.children.length);
+};
