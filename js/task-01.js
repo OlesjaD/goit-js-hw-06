02cc7e2741ccd5e720f82f35cd9faee34850e527
OlesjaD <olesja.d.c@gmail.com>
2023-02-26 21:28:06 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -6,20 +6,20 @@ console.log('Number of categories:', ulElem.length);
 const itemEl = document.querySelector('#categories');
 const firstEl = itemEl.firstElementChild;
 const titlefirstEl = firstEl.firstElementChild;
-console.log(titlefirstEl.textContent);
+console.log('Category:', titlefirstEl.textContent);
 const sibFirstEl = titlefirstEl.nextElementSibling;
-console.log(sibFirstEl.children.length);
+console.log('Elements:', sibFirstEl.children.length);
 
 
 const nextEl = firstEl.nextElementSibling;
 const titlenextEl = nextEl.firstElementChild;
-console.log(titlenextEl.textContent);
+console.log('Category:', titlenextEl.textContent);
 const sibNextEl = titlenextEl.nextElementSibling;
-console.log(sibNextEl.children.length);
+console.log('Elements:', sibNextEl.children.length);
 
 
 const lastEl = itemEl.lastElementChild;
 const titlelastEl = lastEl.firstElementChild;
-console.log(titlelastEl.textContent);
+console.log('Category:', titlelastEl.textContent);
 const sibLastEl = titlelastEl.nextElementSibling;
-console.log(sibLastEl.children.length);
+console.log('Elements:', sibLastEl.children.length);
